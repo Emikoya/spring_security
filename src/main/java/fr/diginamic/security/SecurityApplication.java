2f025c14c8cd5dc5f6e0c55e50477d75017487f3
Emikoya <pruvost.lily@gmail.com>
2025-06-10 10:51:37 +0200

    TP Hash Partie 4.1

--- src/main/java/fr/diginamic/security/SecurityApplication.java
+++ src/main/java/fr/diginamic/security/SecurityApplication.java
@@ -3,11 +3,34 @@ package fr.diginamic.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 @SpringBootApplication
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
+	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
+		//SpringApplication.run(SecurityApplication.class, args);
+
+		String secretKey = "Diginamic c'est chouette";
+		String message = "Voici une chaîne à signer";
+
+		//Crée la clé
+		SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+
+		//Initialise le HMAC
+		Mac mac = Mac.getInstance("HmacSHA256");
+		mac.init(secretKeySpec);
+
+		// Calcule le HMAC
+		byte[] hmacBytes = mac.doFinal(message.getBytes());
+
+		// Encode en Base 64 ou Hex
+		String hmacBase64 = Base64.getEncoder().encodeToString(hmacBytes);
+		System.out.println("Signature HMAC-SHA256 (Base64) : " + hmacBase64);
 	}
 
 }
